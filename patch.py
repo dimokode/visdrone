738873d96d743cd5d795842590c13356e4e73f08
dimokode <dok0032m@gmail.com>
2026-02-08 23:43:58 +0300

    patch.py added

---
+++ patch.py
@@ -0,0 +1,9 @@
+import gdown
+
+def download_weights():
+    url_with_weights = "https://drive.google.com/drive/folders/1TNvJLqJkELUV_5ih9Izdcm8lBc4M_9Em?usp=drive_link"
+
+    gdown.download_folder(url_with_weights, output="./app/weights", quiet=False)
+
+if __name__ == "__main__":
+    download_weights()
