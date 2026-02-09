47bdc32c2841f91378f306451a1c5708ac51f64d
dimokode <dok0032m@gmail.com>
2026-02-09 14:46:27 +0300

    app port changed to 5000

--- app.py
+++ app.py
@@ -337,6 +337,4 @@ def predict():
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(host="0.0.0.0.", port=10000)
-    # gunicorn --bind 0.0.0.0:8000 app:app
-    # app.run()
+    app.run(host="0.0.0.0.", port=5000)
